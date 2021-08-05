05261b452736769d2c128df74e67778bab7e2e9f
LAPTOP-KHDAKGR7\erika <hanerika11@gmail.com>
2021-08-06 00:59:49 +0900

    줄정리

--- src/MyButton.js
+++ src/MyButton.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import { TouchableOpacity, View, Text } from 'react-native';
+import {TouchableOpacity, View, Text} from 'react-native';
 import PropTypes from 'prop-types';
 
-const MyButton = ({title, onPress, children, title2 }) => {
-    return (
-        <TouchableOpacity onPress={onPress}>
-            <View style={{backgroundColor:'red', padding: 10, margin: 10}}>
-                {/* children이 우선순위가 더 높아서 children이 있으면 그걸 출력, 없으면 title */}
-                <Text style={{fontSize: 20, color:'white'}}>{children || title}</Text>
-            </View>
-        </TouchableOpacity>
-    );
+const MyButton = ({title, onPress, children, title2}) => {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View style={{backgroundColor: 'red', padding: 10, margin: 10}}>
+        {/* children이 우선순위가 더 높아서 children이 있으면 그걸 출력, 없으면 title */}
+        <Text style={{fontSize: 20, color: 'white'}}>{children || title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
 };
 
-MyButton.defaultProps={
-    title: 'default',
-    onPress: () => alert('default'),
+MyButton.defaultProps = {
+  title: 'default',
+  onPress: () => alert('default'),
 };
-MyButton.PropTypes={
-    title: PropTypes.string,
-    onPress: PropTypes.func,
-    // isRequired는 필수항목 설정할 때 사용
-    // title2: PropTypes.string.isRequired,
+MyButton.PropTypes = {
+  title: PropTypes.string,
+  onPress: PropTypes.func,
+  // isRequired는 필수항목 설정할 때 사용
+  // title2: PropTypes.string.isRequired,
 };
 
 export default MyButton;
