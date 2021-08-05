63c27513ab419074539c35a950049b7f798a9e00
LAPTOP-KHDAKGR7\erika <hanerika11@gmail.com>
2021-08-06 01:38:02 +0900

    스타일 파일 따로 빼서 사용하기

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {styles, orangeText} from './style';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={[styles.text, {color: 'orange'}]}>Style Code</Text>
+      <Text style={[styles.text, orangeText]}>Style Code</Text>
       <Text style={[styles.text, styles.errorText]}>Error Text</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    backgroundColor: 'black',
-    color: 'white',
-    fontSize: 20,
-  },
-  errorText: {
-    color: 'red',
-  },
-});
