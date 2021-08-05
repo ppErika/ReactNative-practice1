244c2712ec257d89c96cc9fbeaceb3e57b058e50
LAPTOP-KHDAKGR7\erika <hanerika11@gmail.com>
2021-08-06 02:54:44 +0900

    styled-components 활용

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
 import React from 'react';
-import styled, {css} from 'styled-components/native';
+import styled from 'styled-components/native';
+import Input from './Input';
 
 const Container = styled.View`
   flex: 1;
-  background-color: #e3e3e3;
+  background-color: #ffffff;
   align-items: center;
   justify-content: center;
 `;
 
-const cssText = css`
-  font-size: 20px;
-  font-weight: 600;
-`;
-
-const StyledText = styled.Text`
-  ${cssText}
-  color: blue;
-`;
-
-const ErrorText = styled(StyledText)`
-  ${cssText}
-  color: red;
-`;
-const StyledButton = styled.Button``;
-
 export default function App() {
   return (
     <Container>
-      <StyledText>Styled components</StyledText>
-      <ErrorText>Error!!</ErrorText>
-      <StyledButton title="Styled" onPress={() => alert('styled!!')} />
+      <Input />
     </Container>
   );
 }
