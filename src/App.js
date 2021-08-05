ad5e1acdb4d0dcd1d28557c77f02473f28cc5943
LAPTOP-KHDAKGR7\erika <hanerika11@gmail.com>
2021-08-06 01:18:16 +0900

    input onChange이용하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, TextInput, Text} from 'react-native';
 import MyButton from './MyButton';
 
 export default function App() {
@@ -8,10 +8,16 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text style={{fontSize: 20}}>{addition}</Text>
+      <TextInput
+        // onChange={(event) => console.log(event.nativeEvent.text)}
+        // input의 Text만 원하면 onChangeText를 활용하는 것이 더 나음 (위 아래 지금 같은 코드)
+        onChangeText={text=>console.log(text)}
+        style={{borderWidth: 1, padding: 10, fontSize: 20}}
+      />
+      {/* <Text style={{fontSize: 20}}>{addition}</Text>
       <Text style={{fontSize: 20}}>{multiple}</Text>
       <MyButton title="addition" onPress={()=>setAddition(addition+2)}/>
-      <MyButton title="multiple" onPress={()=>setmultiple(multiple*2)}/>
+      <MyButton title="multiple" onPress={()=>setmultiple(multiple*2)}/> */}
     </View>
   );
 }
