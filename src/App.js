166402bd2f2819e006fc85922b5b90dd174bef1c
LAPTOP-KHDAKGR7\erika <hanerika11@gmail.com>
2021-08-06 01:51:03 +0900

    flex는 차지하는 영역을 비율로 나타낼 수 있음 (+ 쓸데없는 파일 지우기)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import {styles, orangeText} from './style';
+import Box from './Box';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={[styles.text, orangeText]}>Style Code</Text>
-      <Text style={[styles.text, styles.errorText]}>Error Text</Text>
+      <Box style={{backgroundColor: 'red', height: 100}} />
+      <Box style={{backgroundColor: 'green', flex: 2}} />
+      <Box style={{backgroundColor: 'blue', flex: 1}} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
