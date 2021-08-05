105b3ada26e74b57d94f36f3724f6fce4b0ed130
LAPTOP-KHDAKGR7\erika <hanerika11@gmail.com>
2021-08-06 03:26:57 +0900

    <ThemeProvider theme={isLight ? lightTheme : darkTheme}> switch버튼에따라 다크테마, 라이트테마 변경하기

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {ThemeProvider} from 'styled-components';
+import {Switch} from 'react-native';
 import styled from 'styled-components/native';
 import Input from './Input';
 
 const Container = styled.View`
   flex: 1;
-  background-color: #ffffff;
+  background-color: ${({theme}) => theme.bgColor};
   align-items: center;
   justify-content: center;
 `;
 
+const lightTheme = {
+  inputColor: '#111111',
+  inputBorder: '#111111',
+  bgColor: '#e3e3e3',
+};
+
+const darkTheme = {
+  inputColor: '#e3e3e3',
+  inputBorder: '#e3e3e3',
+  bgColor: '#111111',
+};
+
 export default function App() {
+  const [isLight, toggleTheme] = useState(true);
   return (
-    <Container>
-      <Input placeholder="Type a message..." />
-      <Input />
-    </Container>
+    <ThemeProvider theme={isLight ? lightTheme : darkTheme}>
+      <Container>
+        <Switch
+          value={isLight}
+          onValueChange={(isLight) => toggleTheme(isLight)}
+        />
+        <Input placeholder="Type a message..." />
+        <Input />
+      </Container>
+    </ThemeProvider>
   );
 }
--- src/Input.js
+++ src/Input.js
@@ -1,13 +1,13 @@
 import React, {useState} from 'react';
 import styled from 'styled-components/native';
 
-const StyledInput = styled.TextInput.attrs(({placeholder}) => ({
+const StyledInput = styled.TextInput.attrs(({placeholder, theme}) => ({
   placeholder: placeholder || 'Enter a message...',
-  placeholderTextColor: '#111111',
+  placeholderTextColor: theme.inputColor,
 }))`
   padding: 20px;
   font-size: 20px;
-  border: 1px solid ${({text}) => (text ? '#00f' : '#111111')};
+  border: 1px solid ${({theme}) => theme.inputBorder};
 `;
 
 const Input = ({placeholder}) => {
