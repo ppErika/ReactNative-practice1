8d25948aa391be1d16c24f8ed5caa6c1eb8ab5ff
LAPTOP-KHDAKGR7\erika <hanerika11@gmail.com>
2021-08-05 22:43:00 +0900

    기본값을 주면 props를 사용하지 않아도 된다

--- src/MyButton.js
+++ src/MyButton.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { TouchableOpacity, View, Text } from 'react-native';
 
-const MyButton = props => {
+const MyButton = ({title='title', onPress=()=>{}, children}) => {
     return (
-        <TouchableOpacity onPress={props.onPress}>
+        <TouchableOpacity onPress={onPress}>
             <View style={{backgroundColor:'red', padding: 10, margin: 10}}>
                 {/* children이 우선순위가 더 높아서 children이 있으면 그걸 출력, 없으면 title */}
-                <Text style={{fontSize: 20, color:'white'}}>{props.children || props.title}</Text>
+                <Text style={{fontSize: 20, color:'white'}}>{children || title}</Text>
             </View>
         </TouchableOpacity>
     );
