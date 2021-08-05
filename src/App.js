5a90d0560915c012351b61dec22eb3a78c751d89
LAPTOP-KHDAKGR7\erika <hanerika11@gmail.com>
2021-08-06 02:41:36 +0900

    styled-components를 이용해서 styled, {css} 적용하기

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
-import Shadow from './Shadow';
+import styled, {css} from 'styled-components/native';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #e3e3e3;
+  align-items: center;
+  justify-content: center;
+`;
+
+const cssText = css`
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+const StyledText = styled.Text`
+  ${cssText}
+  color: blue;
+`;
+
+const ErrorText = styled(StyledText)`
+  ${cssText}
+  color: red;
+`;
+const StyledButton = styled.Button``;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Shadow />
-    </View>
+    <Container>
+      <StyledText>Styled components</StyledText>
+      <ErrorText>Error!!</ErrorText>
+      <StyledButton title="Styled" onPress={() => alert('styled!!')} />
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
