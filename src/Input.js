606c2fdc50fc19e3a52a4e4a798618a9d4b019db
LAPTOP-KHDAKGR7\erika <hanerika11@gmail.com>
2021-08-06 03:02:09 +0900

    attrs를 이용해서 입력값이 있으면 border색이 변하는 input 박스 만들기

--- src/Input.js
+++ src/Input.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components/native';
 
-const StyledInput = styled.TextInput`
+const StyledInput = styled.TextInput.attrs({
+  placeholder: 'Enter a message...',
+  placeholderTextColor: '#111111',
+})`
   padding: 20px;
   font-size: 20px;
-  border: 1px solid #111111;
+  border: 1px solid ${({text}) => (text ? '#00f' : '#111111')};
 `;
 
 const Input = () => {
-  return <StyledInput placeholder="Enter a text..." placeholderTextColor="#111111" />;
+  const [text, setText] = useState('');
+  return <StyledInput onChangeText={(text) => setText(text)} text={text} />;
 };
 
 export default Input;
